8538c2d28491bcd0349e8c2b1831c672b93dfa90
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 12:28:38 -0700

    Create interface for product repository

--- src/main/java/com/example/Project3/repository/ProductRepository.java
+++ src/main/java/com/example/Project3/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.example.Project3.repository;
 
+import com.example.Project3.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByName(String productName);
+
+
 
 }
