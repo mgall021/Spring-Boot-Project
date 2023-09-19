8c1155f19d57ff4ded2c63384eb6acd2e04a8f90
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 00:23:52 -0700

    annotate all variables with the appropriate annotation

--- src/main/java/com/example/Project3/model/Location.java
+++ src/main/java/com/example/Project3/model/Location.java
@@ -1,16 +1,26 @@
 package com.example.Project3.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import javax.persistence.*;
 
-
+@Data // sets up all my setters, getters, To string classes
+@Entity
+@Table(name = "location")
+/**
+ * passes variables id,city,state,and country
+ */
 public class Location {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String city;
+    @Column
     private String state;
+    @Column
     private String country;
 
 
