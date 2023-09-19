5b50ba0ae6aa403d60f38e0d1a72ea1c7cc45f48
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 13:03:56 -0700

    annotate info not found exvception

--- src/main/java/com/example/Project3/exception/InformationNotFoundException.java
+++ src/main/java/com/example/Project3/exception/InformationNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.Project3.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class InformationNotFoundException extends RuntimeException{
     public InformationNotFoundException(String message){
         super(message);
