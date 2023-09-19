9aa4e757ec682a29b558f0a730d8f25d5becdbe1
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 12:38:10 -0700

    create a createProduct Methiod

--- src/main/java/com/example/Project3/controller/ProductController.java
+++ src/main/java/com/example/Project3/controller/ProductController.java
@@ -1,19 +1,21 @@
 package com.example.Project3.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path ="/api")
+
 public class ProductController {
     @GetMapping(path = "/products/")
     public String getProducts(){
         return "get all products";
     }
-@GetMapping(path = "products/{productId}")
+@GetMapping(path = "/products/{productId}")
     public String getProduct(@PathVariable Long productId){
         return "getting the product with the id of" + productId;
+    }
+
+    public String createProduct(@RequestBody String body){
+        return "creating a product" + body;
     }
 }
