19a105c50522c9346d18127b6606e73f4ffc84ba
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 00:16:07 -0700

    create private variables for location model

--- src/main/java/com/example/Project3/model/Customer.java
+++ src/main/java/com/example/Project3/model/Customer.java
@@ -1,4 +1,5 @@
 package com.example.Project3.model;
 
 public class Customer {
+
 }
--- src/main/java/com/example/Project3/model/Location.java
+++ src/main/java/com/example/Project3/model/Location.java
@@ -1,4 +1,18 @@
 package com.example.Project3.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
 public class Location {
+    private Long id;
+    private String city;
+    private String state;
+    private String country;
+
+
+
 }
