e67bf8be411c7f4a9df1275d4c53cb5f615a1316
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 13:03:08 -0700

    pass a super message inside my exception handler

--- src/main/java/com/example/Project3/exception/InformationNotFoundException.java
+++ src/main/java/com/example/Project3/exception/InformationNotFoundException.java
@@ -1,5 +1,7 @@
 package com.example.Project3.exception;
 
 public class InformationNotFoundException extends RuntimeException{
-
+    public InformationNotFoundException(String message){
+        super(message);
+    }
 }
