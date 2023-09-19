e2550a813b624d682c6be1f40a50aa8ab6863742
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 00:55:26 -0700

    create password encyrpter class

--- src/main/java/com/example/Project3/security/SecurityConfiguration.java
+++ src/main/java/com/example/Project3/security/SecurityConfiguration.java
@@ -2,10 +2,14 @@ package com.example.Project3.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Column;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
