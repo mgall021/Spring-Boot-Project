3d048457238b2e4a0fd34abd73abe51ab858d286
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 12:35:58 -0700

    assign GET mappings to the two get Method

--- src/main/java/com/example/Project3/controller/ProductController.java
+++ src/main/java/com/example/Project3/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.Project3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path ="/api")
 public class ProductController {
-
+    @GetMapping(path = "/products/")
     public String getProducts(){
         return "get all products";
     }
-
+@GetMapping(path = "products/{productId}")
     public String getProduct(@PathVariable Long productId){
         return "getting the product with the id of" + productId;
     }
