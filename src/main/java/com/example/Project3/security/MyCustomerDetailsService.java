4b76e3db98ba6be3eed4b95e7c0e2316f69eb32b
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:53:52 -0700

    create userDetail method

--- src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
+++ src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
@@ -1,7 +1,10 @@
 package com.example.Project3.security;
 
+import com.example.Project3.model.Customer;
 import com.example.Project3.service.CustomerService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +14,10 @@ public class MyCustomerDetailsService implements UserDetailsService{
     public MyCustomerDetailsService(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Customer customer = customerService.findCustomerByEmailAddress(email);
+        return new UserDetails(customer);
+    }
+
 }
