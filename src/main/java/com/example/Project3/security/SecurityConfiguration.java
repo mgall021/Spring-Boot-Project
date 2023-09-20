af88fb843415f138cdd20d1ecf54bf6b5b315434
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:18:20 -0700

    create authenticationManager method

--- src/main/java/com/example/Project3/security/SecurityConfiguration.java
+++ src/main/java/com/example/Project3/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.example.Project3.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -45,6 +47,11 @@ public class SecurityConfiguration {
                 .headers().frameOptions().disable();
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
 }
