5cf5899715d887b91438143355a6b4f2db20f8b6
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 23:22:13 -0700

    create annotations for Long id

--- src/main/java/com/example/Project3/model/Customer.java
+++ src/main/java/com/example/Project3/model/Customer.java
@@ -3,19 +3,23 @@ package com.example.Project3.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Data
 @Entity
 @Table(name = "customers")
 public class Customer {
-private Long id;
 
-private String customerName;
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-private String emailAddress;
+    private String customerName;
 
-private String password;
+    private String emailAddress;
+
+    private String password;
 
 
 }
