54bf9a5509caec607f88c711289d69cdca97112f
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:29:40 -0700

    create 2 final variables inside customer service

--- src/main/java/com/example/Project3/service/CustomerService.java
+++ src/main/java/com/example/Project3/service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.example.Project3.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    private final CustomerService customerService;
+    private final PasswordEncoder passwordEncoder;
 
 }
