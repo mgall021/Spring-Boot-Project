ded8b00a4fe35152cf47925c08e2fce8e26721de
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 03:14:35 -0700

    add params to both controller classes

--- src/main/java/com/example/Project3/controller/ProductController.java
+++ src/main/java/com/example/Project3/controller/ProductController.java
@@ -21,12 +21,22 @@ public class ProductController {
     }
 
 //GETALL
+    /**
+     * Retrieve a list of all products.
+     * This endpoint retrieves a list of all available products.
+     * @return A list of Product objects representing the available products.
+     */
 @GetMapping(path = "/products/")
 public List<Product> getProducts(){
     System.out.println("calling get Product");
     return productService.getProducts();
 }
-
+    /**
+     * Retrieve a product by its unique identifier.
+     * This endpoint retrieves a specific product based on its unique identifier.
+     * @param productId The unique identifier of the product to retrieve.
+     * @return An Optional containing the Product object if found, or an empty Optional if not found.
+     */
 @GetMapping(path = "/products/{productId}")
     public Optional<Product> getProduct(@PathVariable Long productId){
     System.out.println("getting the product with the id of" + productId);
@@ -34,19 +44,38 @@ public List<Product> getProducts(){
     }
 
 
-
+    /**
+     * Create a new product.
+     * This endpoint creates a new product by providing the necessary product information
+     * in the request body.
+     * @param productObject The Product object containing the details of the product to be created.
+     * @return The created Product object with its unique identifier assigned.
+     */
 @PostMapping("/products/")
     public Product createProduct(@RequestBody Product productObject){
     System.out.println( "creating a product");
     return productService.createProduct(productObject);
     }
 
-
+    /**
+     * Update an existing product.
+     * This endpoint updates an existing product by providing the product's unique identifier
+     * in the URL path and the updated product details in the request body.
+     * @param productId The unique identifier of the product to update.
+     * @param productObject The Product object containing the updated details of the product.
+     * @return The updated Product object with the specified unique identifier.
+     */
     @PutMapping("/products/{productId}")
     public  Product updateProduct(@PathVariable(value = "productId") Long productId, @RequestBody Product productObject){
         System.out.println(" updating the product with the id of ");
         return productService.updateProduct(productId,productObject);
     }
+    /**
+     * Delete a product by its unique identifier.
+     * This endpoint deletes a specific product based on its unique identifier.
+     * @param productId The unique identifier of the product to delete.
+     * @return An Optional containing the deleted Product object if found and deleted, or an empty Optional if not found.
+     */
 @DeleteMapping("/products/{productId}")
     public Optional<Product> deleteProduct(@PathVariable(value = "productId") Long productId){
     System.out.println( "deleting the product with the id of");
--- src/main/java/com/example/Project3/controller/UserController.java
+++ src/main/java/com/example/Project3/controller/UserController.java
@@ -23,12 +23,25 @@ public class UserController {
         this.customerService = customerService;
     }
 
+    /**
+     * Create a new customer.
+     * This endpoint creates a new customer by providing the necessary customer information
+     * in the request body.
+     * @param customerObject The Customer object containing the details of the customer to be created.
+     * @return The created Customer object with its unique identifier assigned.
+     */
     @PostMapping("/register")
     public Customer createCustomer(@RequestBody Customer customerObject) {
         System.out.println("calling create customer ");
         return customerService.createCustomer(customerObject);
     }
 
+    /**
+     * Log in a customer.
+     * This endpoint allows a customer to log in by providing their login credentials in the request body.
+     * @param loginRequest The LoginRequest object containing the customer's login credentials.
+     * @return An Optional containing an authentication token if login is successful, or an empty Optional if login fails.
+     */
     @PostMapping("/login")
     public Optional<String> loginCustomer(@RequestBody LoginRequest loginRequest){
         System.out.println("calling loginUser ==>");
