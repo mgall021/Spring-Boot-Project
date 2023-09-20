58bcb9c465eca7f2c174f5e62a93f50d1aa4316c
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 23:35:55 -0700

    annoate long id inside profile class

--- src/main/java/com/example/Project3/model/CustomerProfile.java
+++ src/main/java/com/example/Project3/model/CustomerProfile.java
@@ -1,12 +1,13 @@
 package com.example.Project3.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "profiles")
 public class CustomerProfile {
-
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
