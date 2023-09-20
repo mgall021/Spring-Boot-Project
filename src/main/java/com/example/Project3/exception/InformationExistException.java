428cb066fbfc21e1e7c223b89840665e9724ed61
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 03:16:23 -0700

    add params to both exception classes

--- src/main/java/com/example/Project3/exception/InformationExistException.java
+++ src/main/java/com/example/Project3/exception/InformationExistException.java
@@ -3,6 +3,12 @@ package com.example.Project3.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception indicating that the requested information already exists and conflicts with existing data.
+ * This exception is thrown when an operation attempts to create or update data that already exists,
+ * causing a conflict with the existing data.
+ * @param message A custom error message describing the conflict or reason for the exception.
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
     public InformationExistException(String message){
--- src/main/java/com/example/Project3/exception/InformationNotFoundException.java
+++ src/main/java/com/example/Project3/exception/InformationNotFoundException.java
@@ -3,6 +3,13 @@ package com.example.Project3.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception indicating that the requested information was not found.
+ * This exception is thrown when an operation attempts to access or retrieve information that does not exist,
+ * causing a conflict due to the absence of the requested data.
+ * @param message A custom error message describing the reason for the information not being found.
+ */
+
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationNotFoundException extends RuntimeException{
     public InformationNotFoundException(String message){
