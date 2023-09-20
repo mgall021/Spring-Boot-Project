ade033b9fb464185b70515eb500e60ddc93d55c6
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:43:04 -0700

    create variables and getters for a login request

--- src/main/java/com/example/Project3/model/request/LoginRequest.java
+++ src/main/java/com/example/Project3/model/request/LoginRequest.java
@@ -1,4 +1,14 @@
 package com.example.Project3.model.request;
 
 public class LoginRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
