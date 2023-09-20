682f836d385898967aa420aa8b2828ff43ee6f76
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:17:35 -0700

    create securityfilter chain method

--- src/main/java/com/example/Project3/security/SecurityConfiguration.java
+++ src/main/java/com/example/Project3/security/SecurityConfiguration.java
@@ -3,7 +3,11 @@ package com.example.Project3.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.persistence.Column;
 
@@ -29,5 +33,18 @@ public class SecurityConfiguration {
         return new JwtRequestFilter();
     }
 
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/customers", "/auth/customers/login/", "/auth/customers/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 }
