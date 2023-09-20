610a2f55946300e0db0662bf0c54db288b4002d3
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:50:34 -0700

    create variable and constructor for Mycustomerdetailservice class

--- src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
+++ src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
@@ -1,9 +1,14 @@
 package com.example.Project3.security;
 
+import com.example.Project3.service.CustomerService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyCustomerDetailsService implements UserDetailsService {
+public class MyCustomerDetailsService implements UserDetailsService{
+    private CustomerService customerService;
 
+    public MyCustomerDetailsService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }
