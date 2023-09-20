0ce4b0662bb54cf16e153b7849aa788cffb7d9c3
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 03:22:13 -0700

    add params to both repository classes

--- src/main/java/com/example/Project3/repository/CustomerRepository.java
+++ src/main/java/com/example/Project3/repository/CustomerRepository.java
@@ -3,7 +3,11 @@ package com.example.Project3.repository;
 import com.example.Project3.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Repository interface for managing customer data.
+ * This repository interface provides methods for performing database operations related to customer entities.
+ * It extends JpaRepository to leverage Spring Data JPA functionality for common CRUD operations.
+ */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmailAddress(String customerEmailAddress);
--- src/main/java/com/example/Project3/repository/ProductRepository.java
+++ src/main/java/com/example/Project3/repository/ProductRepository.java
@@ -3,7 +3,11 @@ package com.example.Project3.repository;
 import com.example.Project3.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Repository interface for managing product data.
+ * This repository interface provides methods for performing database operations related to product entities.
+ * It extends JpaRepository to leverage Spring Data JPA functionality for common CRUD operations.
+ */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByName(String productName);
