f1aface3e7ee726d85b640710347ce848d3fc1f4
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 03:37:12 -0700

    add params to customer details service

--- src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
+++ src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
@@ -8,6 +8,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * Custom UserDetailsService implementation for loading customer details.
+ * This class implements the UserDetailsService interface to provide a custom implementation for loading customer details
+ * during authentication. It retrieves a Customer object by email address from the CustomerService and wraps it in
+ * a MyCustomerDetails object for authentication and authorization purposes.
+ */
 @Service
 public class MyCustomerDetailsService implements UserDetailsService{
     private CustomerService customerService;
@@ -15,6 +21,13 @@ public class MyCustomerDetailsService implements UserDetailsService{
     public MyCustomerDetailsService(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    /**
+     * Load customer details by email address for authentication.
+     * @param email The email address used to identify the customer.
+     * @return A UserDetails object representing the customer's details.
+     * @throws UsernameNotFoundException If the customer with the specified email address is not found.
+     */
 @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Customer customer = customerService.findCustomerByEmailAddress(email);
