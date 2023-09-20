1a98de0acc564674f6d55acbbe3b91b1f2cc6d95
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:37:41 -0700

    create a createCustomer method inside service

--- src/main/java/com/example/Project3/service/CustomerService.java
+++ src/main/java/com/example/Project3/service/CustomerService.java
@@ -1,6 +1,9 @@
 package com.example.Project3.service;
 
+import com.example.Project3.exception.InformationExistException;
+import com.example.Project3.model.Customer;
 import com.example.Project3.repository.CustomerRepository;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,24 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public CustomerService(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+    public CustomerService(CustomerRepository customerRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
         this.passwordEncoder = passwordEncoder;
+
     }
+
+    public Customer createCustomer(Customer customerObject) {
+        System.out.println("service calling create Customerr ");
+        if (!customerRepository.existsByEmailAddress(customerObject.getEmailAddress())) {
+            customerObject.setPassword(passwordEncoder.encode(customerObject.getPassword()));
+            return customerRepository.save(customerObject);
+        } else {
+            throw new InformationExistException("customer with email address " + customerObject.getEmailAddress() +
+                    " already exists");
+        }
+    }
+
+
+
+
 }
