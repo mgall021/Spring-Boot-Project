e5620fc8ba2da3bf3dc731e0bd8bfbda1e5fc850
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:57:41 -0700

    add annotattions to customerdetailservice class

--- src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
+++ src/main/java/com/example/Project3/security/MyCustomerDetailsService.java
@@ -2,6 +2,7 @@ package com.example.Project3.security;
 
 import com.example.Project3.model.Customer;
 import com.example.Project3.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyCustomerDetailsService implements UserDetailsService{
     private CustomerService customerService;
-
+@Autowired
     public MyCustomerDetailsService(CustomerService customerService) {
         this.customerService = customerService;
     }
-
+@Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Customer customer = customerService.findCustomerByEmailAddress(email);
-        return new UserDetails(customer);
+        return new MyCustomerDetails(customer);
     }
 
 }
