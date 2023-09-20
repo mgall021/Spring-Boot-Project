b15bdeeb80dd4ef530517ec3e28d841359a41c19
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:29:31 -0700

    create pareeJwt method

--- src/main/java/com/example/Project3/security/JwtRequestFilter.java
+++ src/main/java/com/example/Project3/security/JwtRequestFilter.java
@@ -2,8 +2,10 @@ package com.example.Project3.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -16,9 +18,18 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     public void setMyCustomerDetailsService(MyCustomerDetailsService myCustomerDetailsService){
         this.myCustomerDetailsService=myCustomerDetailsService;
     }
-@Autowired
+    @Autowired
     public void setJwtUtils(JWTUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        // Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXJlc2gyQGdhLmNvbSIsImlhdCI6MTY5NDgwMDAzNiwiZXhwIjoxNjk0ODg2NDM2fQ.z3smvkvDJqOYz7699UjvH5JQ51MuWL-KXffegc1UxWU
+        if (StringUtils.hasLength("headerAuth") && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7);
+        }
+        return null;
     }
 
 
