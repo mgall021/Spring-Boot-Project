86c8cd7449e703ca8d3ab4ce4708369d5f6655f5
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:35:44 -0700

    create doFilterInternal method

--- src/main/java/com/example/Project3/security/JwtRequestFilter.java
+++ src/main/java/com/example/Project3/security/JwtRequestFilter.java
@@ -1,11 +1,19 @@
 package com.example.Project3.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @Component
@@ -31,6 +39,24 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
         return null;
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String customerName = jwtUtils.getCustomerNameFromJwtToken(jwt);
+                UserDetails userDetails = this.myCustomerDetailsService.loadUserByUsername(customerName); // email address
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.info("cannot set user authentication token");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
 
 
 }
