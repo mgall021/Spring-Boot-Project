d0e7e540d2592b3b28c0b358d07297dea0165084
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 00:31:46 -0700

    fix error where CustomerRepo was labeled as as ervice

--- src/main/java/com/example/Project3/service/CustomerService.java
+++ src/main/java/com/example/Project3/service/CustomerService.java
@@ -1,11 +1,13 @@
 package com.example.Project3.service;
 
+import com.example.Project3.repository.CustomerRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-    private final CustomerService customerService;
+    private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
+
 
 }
