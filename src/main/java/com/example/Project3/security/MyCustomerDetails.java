f3945a77ed649a0c1e3bb6de27e71203a0e7eae1
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-20 01:02:24 -0700

    connect userdetails to user service by overwriting getters, setters

--- src/main/java/com/example/Project3/security/MyCustomerDetails.java
+++ src/main/java/com/example/Project3/security/MyCustomerDetails.java
@@ -1,7 +1,54 @@
 package com.example.Project3.security;
 
+import com.example.Project3.model.Customer;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class MyCustomerDetails implements UserDetails {
+import java.util.Collection;
+import java.util.HashSet;
 
+public class MyCustomerDetails implements UserDetails {
+    private Customer customer;
+    public MyCustomerDetails(Customer customer){
+        this.customer=customer;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return customer.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return customer.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Customer getUser() {
+        return customer;
+    }
 }
+
