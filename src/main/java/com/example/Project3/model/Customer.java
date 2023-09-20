0e53ec46e1dab42ab0b2478ee478e92db63e56c6
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 23:19:06 -0700

    create variables for customer class

--- src/main/java/com/example/Project3/model/Customer.java
+++ src/main/java/com/example/Project3/model/Customer.java
@@ -7,7 +7,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "customers")
 public class Customer {
+private Long id;
 
+private String customerName;
+
+private String emailAddress;
+
+private String password;
 
 
 }
