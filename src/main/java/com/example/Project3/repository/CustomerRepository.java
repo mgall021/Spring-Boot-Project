c3b510f4dec6f58e3a9b98aeb95d81d84573f4a7
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-19 23:56:36 -0700

    add find and exists by email methods

--- src/main/java/com/example/Project3/repository/CustomerRepository.java
+++ src/main/java/com/example/Project3/repository/CustomerRepository.java
@@ -1,7 +1,13 @@
 package com.example.Project3.repository;
 
+import com.example.Project3.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CustomerRepository {
+public class CustomerRepository extends JpaRepository<Customer, long> {
+    boolean existsByEmailAddress(String customerEmailAddress);
+
+    Customer findUserByEmailAddress(String emailAddress) ;// to login
 }
+
